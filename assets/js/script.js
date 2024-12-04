5821ec2eebc5bc1ab14b513c2a96cb2022565ee9
Adam Campbell <adamcampbelldev@gmail.com>
2024-12-04 18:35:09 +0000

    refactor to mvc pattern

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,142 +1,193 @@
 /**
- * Description of behaviour
- * 
- * There are four categories of questions - addition, subtraction, multiplication, and division.
- * The user is contiuously asked questions for the currently selected category. Each question is randomly
- * generated, consisting of two randomly selected operands, with the relevant operator. 
- * 
- * When the user submits the correct answer, the users correct answers count is incremented by 1, and when
- * the user submits the incorrect answer, the users incorrect answers count is incremented by 1.
- * 
- * There is no end  - the user can continue answering questions until they decide to stop, switching between
- * categories whenever they wish.
- * 
- * 
- * 
+ * Curried function, when fully applied, returns a random integer between min and max.
  */
+const getRandomIntFromMinToMax = min => max => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+/**
+ * Returns a random integer between 1 and max
+ */
+const getRandomInt = getRandomIntFromMinToMax(1);
+
+/**
+ * Returns a random integer between 2 and max
+ */
+const getRandomIntGT1 = getRandomIntFromMinToMax(2);
 
 
-console.log("Aloha, world!");
+/**
+ * Model object that represents the state of the application.
+ * Note, the value of the answer input element is not stored as part of the application state.
+ * The application state updates when a new question is generated or when the user submits an answer.
+ */
+const model = {
+    currentCategory: "addition",
+    leftOperandValue: 0,
+    rightOperandValue: 0,
+    correctAnswer: 0,
+    numCorrectAnswers: 0,
+    numIncorrectAnswers: 0,
+    /**
+     * Returns the current state of the application.
+     */
+    getState() {
+        return {
+            currentCategory: this.currentCategory,
+            leftOperandValue: this.leftOperandValue,
+            rightOperandValue: this.rightOperandValue,
+            correctAnswer: this.correctAnswer,
+            numCorrectAnswers: this.numCorrectAnswers,
+            numIncorrectAnswers: this.numIncorrectAnswers
+        };
+    },
+    /**
+     * Updates state of application with new question based on category.
+     */
+    setupQuestion(category) {
+        this.currentCategory = category;
+        switch (category) {
+            case "addition":
+                this.leftOperandValue = getRandomInt(50);
+                this.rightOperandValue = getRandomInt(50);
+                this.correctAnswer = this.leftOperandValue + this.rightOperandValue;
+                break;
+            case "subtract":
+                let subtractTemp1 = getRandomInt(50);
+                let subtractTemp2 = getRandomInt(50);
+                this.leftOperandValue = Math.max(subtractTemp1, subtractTemp2);
+                this.rightOperandValue = Math.min(subtractTemp1, subtractTemp2);
+                this.correctAnswer = this.leftOperandValue - this.rightOperandValue;
+                break;
+            case "multiply":
+                this.leftOperandValue = getRandomIntGT1(20);
+                this.rightOperandValue = getRandomIntGT1(20);
+                this.correctAnswer = this.leftOperandValue * this.rightOperandValue;
+                break;
+            case "division":
+                let divisionTemp1 = getRandomIntGT1(20);
+                let divisionTemp2 = getRandomIntGT1(20);
+                let product = divisionTemp1 * divisionTemp2;
+                this.leftOperandValue = product;
+                this.rightOperandValue = divisionTemp1;
+                this.correctAnswer = divisionTemp2;
+                break;
+        }   
+    },
+    /**
+     * Updates the state of the application with the user's answer.
+     */
+    submitAnswer(userAnswer) {
+        if (userAnswer === this.correctAnswer) {
+            this.numCorrectAnswers++;
+        } else {
+            this.numIncorrectAnswers++;
+        }
+        this.setupQuestion(this.currentCategory);
+    }
+};
 
-// Element selectors
-const leftOperand = document.getElementById("operand1");
-const rightOperand = document.getElementById("operand2");
-const operator = document.getElementById("operator");
-const answerBox = document.getElementById("answer-box");
-const correctAnswerScore = document.getElementById("score");
-const incorrectAnswerScore = document.getElementById("incorrect");
-const additionButton = document.querySelector('button[data-type="addition"]');
-const subtractionButton = document.querySelector('button[data-type="subtract"]');
-const multiplicationButton = document.querySelector('button[data-type="multiply"]');
-const divisionButton = document.querySelector('button[data-type="division"]');
-const submitButton = document.querySelector('button[data-type="submit"]');
 
-// other global variables
-let correctAnswers = 0;
-let incorrectAnswers = 0;
-let currentCategory = "addition";
-let leftOperandValue = 0;
-let rightOperandValue = 0;
-let correctAnswerValue = 0;
-
-
-// user selects a category
-// random operands are generated, and the answer is calculated and stored
-// view is updated to show question
-// user submits answer, answer is checked against stored answer
-// dialog is displayed to show if answer is correct or incorrect, and counters are updated
-// new question in the current category is generated, view updated, steps are repeated
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max) + 1;
+/**
+ * View object that represents the UI of the application.
+ */
+const view = {
+    leftOperandEl: document.getElementById("operand1"),
+    rightOperandEl: document.getElementById("operand2"),
+    operatorEl: document.getElementById("operator"),
+    answerBoxEl: document.getElementById("answer-box"),
+    correctAnswerScoreEl: document.getElementById("score"),
+    incorrectAnswerScoreEl: document.getElementById("incorrect"),
+    additionButtonEl: document.querySelector('button[data-type="addition"]'),
+    subtractionButtonEl: document.querySelector('button[data-type="subtract"]'),
+    multiplicationButtonEl: document.querySelector('button[data-type="multiply"]'),
+    divisionButtonEl: document.querySelector('button[data-type="division"]'),
+    submitButtonEl: document.querySelector('button[data-type="submit"]'),
+    /**
+     * Maps categories to their respective operators, only for internal use by view object.
+     * @param {*} category 
+     * @returns 
+     */
+    _getOperator(category) {
+        switch (category) {
+            case "addition":
+                return "+";
+            case "subtract":
+                return "-";
+            case "multiply":
+                return "x";
+            case "division":
+                return "÷";
+            default:
+                return "+";
+        }
+    },
+    /**
+     * Updates the DOM with the current state of the application.
+     */
+    render(state) {
+        this.leftOperandEl.textContent = state.leftOperandValue;
+        this.rightOperandEl.textContent = state.rightOperandValue;
+        this.operatorEl.textContent = this._getOperator(state.currentCategory);
+        this.correctAnswerScoreEl.textContent = state.numCorrectAnswers;
+        this.incorrectAnswerScoreEl.textContent = state.numIncorrectAnswers;
+        this.answerBoxEl.value = "";
+        this.answerBoxEl.focus();
+    }
 }
 
 
-function setupQuestion(category) {
-    answerBox.value = "";
-    currentCategory = category;
-    console.log(`Category selected: ${currentCategory}`);
-    
-    if (category === "addition") {
-        operator.textContent = "+";
-        leftOperandValue = getRandomInt(50);
-        rightOperandValue = getRandomInt(50);
-        correctAnswerValue = leftOperandValue + rightOperandValue;
-    } else if (category === "subtract") {
-        operator.textContent = "-";
-        let num1 = getRandomInt(50);
-        let num2 = getRandomInt(50);
-        leftOperandValue = Math.max(num1, num2);
-        rightOperandValue = Math.min(num1, num2);
-        correctAnswerValue = leftOperandValue - rightOperandValue;
-    } else if (category === "multiply") {
-        operator.textContent = "x";
-        leftOperandValue = getRandomInt(19) + 1;
-        rightOperandValue = getRandomInt(19) + 1;
-        correctAnswerValue = leftOperandValue * rightOperandValue;
-    } else {
-        operator.textContent = "÷";
-        let num1 = getRandomInt(19) + 1;
-        let num2 = getRandomInt(19) + 1;
-        let product = num1 * num2;
-        leftOperandValue = product;
-        rightOperandValue = num1;
-        correctAnswerValue = num2;
+/**
+ * Controller object that connects the model and view objects.
+ */
+const controller = {
+    answerBoxEl: document.getElementById("answer-box"),
+    additionButtonEl: document.querySelector('button[data-type="addition"]'),
+    subtractionButtonEl: document.querySelector('button[data-type="subtract"]'),
+    multiplicationButtonEl: document.querySelector('button[data-type="multiply"]'),
+    divisionButtonEl: document.querySelector('button[data-type="division"]'),
+    answerBoxEl: document.getElementById("answer-box"),
+    submitButtonEl: document.querySelector('button[data-type="submit"]'),
+    /**
+     * Initializes the application.
+     */
+    init() {
+        this.setupEventListeners();
+        model.setupQuestion("addition");
+        view.render(model.getState());
+    },
+    /**
+     * Attaches event listeners to the UI elements.
+     */
+    setupEventListeners() {
+        this.additionButtonEl.addEventListener("click", e => this.setupQuestion("addition"));
+        this.subtractionButtonEl.addEventListener("click", e => this.setupQuestion("subtract"));
+        this.multiplicationButtonEl.addEventListener("click", e => this.setupQuestion("multiply"));
+        this.divisionButtonEl.addEventListener("click", e => this.setupQuestion("division"));
+        this.submitButtonEl.addEventListener("click", e => this.submitAnswer());
+        this.answerBoxEl.addEventListener("keydown", e => {
+            if (e.key === "Enter") {
+                this.submitAnswer();
+            }
+        });
+    },
+    /**
+     * Sets up a new question by updating the model and re-rendering the view.
+     */
+    setupQuestion(category) {
+        model.setupQuestion(category);
+        view.render(model.getState());
+    },
+    /**
+     * Submits the user's answer by updating the model and re-rendering the view.
+     */
+    submitAnswer() {
+        model.submitAnswer(parseInt(this.answerBoxEl.value));
+        view.render(model.getState());
     }
-    leftOperand.textContent = leftOperandValue;
-    rightOperand.textContent = rightOperandValue;
-    answerBox.focus();
 }
 
-
-function submitAnswer() {
-    
-    const userAnswer = parseInt(answerBox.value);
-    if (userAnswer === correctAnswerValue) {
-        console.log("You are correct!");
-        correctAnswers++;
-        correctAnswerScore.textContent = correctAnswers;
-        
-    } else {
-        console.log("You are incorrect!");
-        incorrectAnswers++;
-        incorrectAnswerScore.textContent = incorrectAnswers;
-    }
-    setupQuestion(currentCategory);
-
-}
-
-
-
-additionButton.addEventListener("click", e => setupQuestion("addition"));
-subtractionButton.addEventListener("click", e => setupQuestion("subtract"));
-multiplicationButton.addEventListener("click", e => setupQuestion("multiply"));
-divisionButton.addEventListener("click", e => setupQuestion("division"));
-
-submitButton.addEventListener("click", submitAnswer);
-answerBox.addEventListener("keydown", e => {
-    if (e.key === "Enter") {
-        submitAnswer();
-    }
-});
-
-
-
-
-
-// Setup the first question
-setupQuestion("addition");
-
-
-
-
-
-
-
-
-
-
-
+controller.init();
 
 
 
